3835be8e2fe8f7c0aac301e378d228f76986bdcf
psyitama <psyrone.agpalo@yahoo.com>
2021-06-08 20:26:06 +0800

    initial commit

--- models/git-command.js
+++ models/git-command.js
@@ -1,54 +1,77 @@
 class GitCommand {
-    constructor(working_directory){
+    constructor(working_directory) {
         this.working_directory = working_directory;
     }
-    //Command: git init 
-    init(){
+    //Command: git init
+    init() {
         this.staging = [];
         this.local_repository = [];
-        return "Initialized as empty Git repository.";
+        return 'Initialized as empty Git repository.';
     }
 
     //Command: git status
-    // status(){}
+    status() {
+        let status = '';
+        let files = '';
+        let counter = 0;
+        for (const property in this.working_directory.new_changes) {
+            counter++;
+            files += `\n${property}`;
+        }
 
-    //Command: git add <filename/file directory/wildcard> 
-    add(path_file){
+        if (counter == 0) {
+            status = `You have ${counter} change/s.\n`;
+        } else {
+            status = `You have ${counter} change/s.${files}`;
+        }
+        return status;
+    }
+
+    //Command: git add <filename/file directory/wildcard>
+    add(path_file) {
         let modified_files = this.working_directory.new_changes;
-        
-        if(modified_files[path_file]){
+
+        if (modified_files[path_file]) {
             this.staging.push(modified_files[path_file]);
             delete modified_files[path_file];
-        }
-        /*
-            Create logic here and run unit testing.
-        */
-        else{
+        } else if (path_file == '.') {
+            this.staging.push(modified_files);
+            this.working_directory.new_changes = {};
+        } else if (path_file == '*') {
+            let file_list = Object.keys(modified_files);
+            for (let row = 0; row < file_list.length; row++) {
+                if (!file_list[row].startsWith('.')) {
+                    this.staging.push(modified_files[file_list[row]]);
+                    delete this.working_directory.new_changes[file_list[row]];
+                }
+            }
+        } else {
             return `Failed to add ${path_file}! File is not modified or missing.`;
         }
-        return "Successfully added as index file/s.";
+        return 'Successfully added as index file/s.';
     }
 
     //Command: git commit -m "<message>"
-    commit(message){
-        if(this.staging.length > 0){
-            this.local_repository.push({ "message": message, "files": this.staging });
+    commit(message) {
+        if (this.staging.length > 0) {
+            this.local_repository.push({
+                message: message,
+                files: this.staging
+            });
             this.staging = [];
-            return "Done committing to local repository.";
+            return 'Done committing to local repository.';
         }
-        return "Nothing to commit.";
+        return 'Nothing to commit.';
     }
 
     //Command: git push
-    push(){   
-        if(this.local_repository.length > 0){
-            return "Done pushing to remote repository.";
-        } 
-        else {
-            return "Nothing to push. No committed file found.";
-        }     
+    push() {
+        if (this.local_repository.length > 0) {
+            return 'Done pushing to remote repository.';
+        } else {
+            return 'Nothing to push. No committed file found.';
+        }
     }
 }
-
 
 module.exports = GitCommand;
